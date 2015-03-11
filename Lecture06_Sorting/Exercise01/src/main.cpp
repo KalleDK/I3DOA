bdee6e048db5c4ed80f50928ed95e3d0490e7700
KalleDK <github.com@k-moeller.dk>
2015-03-11 15:45:00 +0100

    function

--- Lecture06_Sorting/Exercise01/src/main.cpp
+++ Lecture06_Sorting/Exercise01/src/main.cpp
@@ -3,16 +3,9 @@
 
 #define ARRAY_SIZE 20
 
-int main(void) {
-	srand (time(NULL));
-	int arr[ARRAY_SIZE];
+void insertionsort(int* arr, int size) {
 	int mover;
-	for (int i = 0; i < ARRAY_SIZE; ++i) {
-		arr[i] = rand() % 100;
-		std::cout << arr[i] << " ";
-	}
-	std::cout << "\n";
-	for (int i = 1; i < ARRAY_SIZE; ++i) {
+	for (int i = 1; i < size; ++i) {
 		int j = i;
 		while(j > 0 && arr[j-1] > arr[j]) {
 			mover = arr[j-1];
@@ -20,8 +13,18 @@ int main(void) {
 			arr[j] = mover;
 			--j;
 		}
-		
 	}
+}
+
+int main(void) {
+	srand (time(NULL));
+	int arr[ARRAY_SIZE];
+	for (int i = 0; i < ARRAY_SIZE; ++i) {
+		arr[i] = rand() % 100;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << "\n";
+	insertionsort(arr, ARRAY_SIZE);
 	for (int i = 0; i < ARRAY_SIZE; ++i) {
 		std::cout << arr[i] << " ";
 	}
