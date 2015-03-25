480121df1e54dccd965aaf39b512a2f1a805efa7
20105969 <20105969@post.au.dk>
2015-03-25 21:25:32 +0100

    rotate lr

--- Lecture08_AVLTree/host/main.d
+++
@@ -1,2 +0,0 @@
-host/main.o: src/main.cpp src/BTNode.hpp src/BTNodeToolkit.hpp
-	${CXX} -c -o $@ $< ${CXXFLAGS}
--- Lecture08_AVLTree/src/BTNodeToolkit.hpp
+++ Lecture08_AVLTree/src/BTNodeToolkit.hpp
@@ -1,18 +1,83 @@
 #pragma once
 
 #include <iostream>
+#include <algorithm>
 #include "BTNode.hpp"
 
 template <class T>
 class BTNodeToolkit
 {
 public:
+	static int getDepth(BTNode<T>*& root)
+	{
+		if (root == NULL)
+			return 0;
+		return 1 + std::max(getDepth(root->left), getDepth(root->right));
+	}
+	// Root != NULL
+	// Root->left != NULL
+	static BTNode<T>* rotateRight(BTNode<T>* root)
+	{
+		// Move old root parent to new root parent
+		root->left->parent = root->parent;
+		// Move new root to old root parent
+		root->parent = root->left;
+		// Move new root right to old root left
+		root->left = root->parent->right;
+		// Move old root to new root right
+		root->parent->right = root;
+
+		// Update moved node
+		if (root->left != NULL) {
+			root->left->parent = root->right->left;
+		}
+
+		// Update old parent
+		if (root->parent->parent != NULL) {
+			if (root->parent->parent->right == root->parent->right) {
+				root->parent->parent->right = root->parent;
+			} else {
+				root->parent->parent->left = root->parent;
+			}
+		}
+
+		return root->parent;
+	}
+
+	static BTNode<T>* rotateLeft(BTNode<T>* root)
+	{
+		// Move old root parent to new root parent
+		root->right->parent = root->parent;
+		// Move new root to old root parent
+		root->parent = root->right;
+		// Move new root left to old root right
+		root->right = root->parent->left;
+		// Move old root to new root left
+		root->parent->left = root;
+
+		// Update moved node
+		if (root->right != NULL) {
+			root->right->parent = root->left->right;
+		}
+
+		// Update old parent
+		if (root->parent->parent != NULL) {
+			if (root->parent->parent->right == root->parent->left) {
+				root->parent->parent->right = root->parent;
+			} else {
+				root->parent->parent->left = root->parent;
+			}
+		}
+
+		return root->parent;
+	}
+
 	static void insert(BTNode<T>*& root, const T& data)
 	{
 		if (root == NULL) {
 			root = new BTNode<T>(data);
 		}
-		else if (data <= root->data) {
+		else if (data < root->data) {
 			if (root->left == NULL) {
 				root->left = new BTNode<T>(data);
 				root->left->parent = root;
@@ -20,7 +85,7 @@ public:
 				insert(root->left, data);
 			}
 		}
-		else {
+		else if (data > root->data) {
 			if (root->right == NULL) {
 				root->right = new BTNode<T>(data);
 				root->right->parent = root;
@@ -57,8 +122,11 @@ public:
 
 	static void print(BTNode<T>* root, int depth = 0)
 	{
+		char y;
 		if (root == NULL)
+		{
 			return;
+		}
 		print(root->right, depth + 1);
 		std::cout << std::string(depth*3, ' ') << root->data << "\n";
 		print(root->left, depth + 1);
